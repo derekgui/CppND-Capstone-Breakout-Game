a0894e8bed962f0cf17c3da37984f3add5dd96ca
Derek Gui <derek.guiz@gmail.com>
2019-08-15 12:14:17 +1200

    Test Drived the IsActive method.

--- test/testGTest.cpp
+++ test/testGTest.cpp
@@ -1,7 +1,41 @@
+#include "SDL2/SDL.h"
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+using namespace ::testing;
+using ::testing::Eq;
+using ::testing::Return;
 
-TEST(testGTest, verifyGtest)
+class MainWindow
 {
-    EXPECT_TRUE(false) << "Verify successed!";
+public:
+    void init()
+    {
+        m_isActive = true;
+    }
+
+    bool isActive()
+    {
+        return m_isActive;
+    }
+
+private:
+    bool m_isActive{false};
+};
+
+class MainGameWindow : public testing::Test
+{
+public:
+    MainWindow wnd;
+};
+
+TEST_F(MainGameWindow, IsDeactiveByDefaultAfterCreate)
+{
+
+    ASSERT_THAT(wnd.isActive(), false);
+}
+
+TEST_F(MainGameWindow, IsActiveAfterInitialization)
+{
+    wnd.init();
+    ASSERT_THAT(wnd.isActive(), true);
 }
