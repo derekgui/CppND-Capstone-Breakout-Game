afc4abbf2c1ee9a521f28f862805cea925f521c6
Derek Gui <derek.guiz@gmail.com>
2019-08-23 12:59:42 +1200

    Test driven checkCollision method of Ball class.

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -7,4 +7,24 @@ Ball::Ball(SDL_Rect brickBlock, Color c, bool isFixed)
 
 bool Ball::checkCollision(const BaseBlock &block)
 {
+    int ball_top = getBlock().y;
+    int ball_bottom = getBlock().y + getBlock().h;
+    int ball_left = getBlock().x;
+    int ball_right = getBlock().x + getBlock().w;
+
+    int block_top = block.getBlock().y;
+    int block_bottom = block.getBlock().y + block.getBlock().h;
+    int block_left = block.getBlock().x;
+    int block_right = block.getBlock().x + block.getBlock().w;
+
+    if (ball_bottom <= block_top)
+        return false;
+    if (ball_top >= block_bottom)
+        return false;
+    if (ball_right <= block_left)
+        return false;
+    if (ball_left >= block_right)
+        return false;
+
+    return true;
 }
--- test/testBall.cpp
+++ test/testBall.cpp
@@ -1,4 +1,5 @@
 #include "../src/MainWindow.cpp"
+#include "../src/Controller.cpp"
 #include "../src/Renderer.cpp"
 #include "../src/Paddle.cpp"
 #include "../src/BaseBlock.cpp"
@@ -22,11 +23,42 @@ TEST_F(GameBall, BallBlockInitializedAfterCreation)
     ASSERT_THAT(ball.getBlock(), EqBlock(testBlock));
 }
 
-TEST_F(GameBall, IsNotCollideIfItsBottomPostionGreaterThanAnothersTop)
+TEST_F(GameBall, IsNotCollideIfItsBottomlessThanAnothersTop)
 {
     SDL_Rect paddleBlock{0, 64, 32 * 6, 32};
 
     Paddle paddle{paddleBlock, Colors::Red, false};
 
-    ASSERT_THAT(ball.checkCollision(paddle), Eq(true));
+    ASSERT_THAT(ball.checkCollision(paddle), Eq(false));
+}
+
+TEST_F(GameBall, IsNotCollideIfItsTopGreatThanAnothersBottom)
+{
+    ball.setBlock().y = 64;
+
+    SDL_Rect paddleBlock{0, 0, 32 * 6, 32};
+
+    Paddle paddle{paddleBlock, Colors::Red, false};
+
+    ASSERT_THAT(ball.checkCollision(paddle), Eq(false));
+}
+
+TEST_F(GameBall, IsNotCollideIfItsRightLessThanAnothersLeft)
+{
+    SDL_Rect paddleBlock{64, 0, 32 * 6, 32};
+
+    Paddle paddle{paddleBlock, Colors::Red, false};
+
+    ASSERT_THAT(ball.checkCollision(paddle), Eq(false));
+}
+
+TEST_F(GameBall, IsNotCollideIfItsLeftGreatThanAnothersRight)
+{
+    ball.setBlock().x = 32 * 6 + 1;
+
+    SDL_Rect paddleBlock{0, 0, 32 * 6, 32};
+
+    Paddle paddle{paddleBlock, Colors::Red, false};
+
+    ASSERT_THAT(ball.checkCollision(paddle), Eq(false));
 }
